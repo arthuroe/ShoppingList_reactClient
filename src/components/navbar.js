f9fbcb6bad36ae15f1821003539cdf45515ef09e
orache arthur <arthur.orache@gmail.com>
2017-12-20 22:40:11 +0300

    Fix links issue

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,9 +13,9 @@ class Navbar extends Component {
       <div>
         <nav className="blue darken-3">
           <div className="nav-wrapper">
-            <Link to="/shoppinglists" className="brand-logo center">
+            <a href="/shoppinglists" className="brand-logo center">
               Shoppinglists
-            </Link>
+            </a>
             <a
               data-activates="main-menu"
               className="button-collapse show-on-large"
@@ -24,27 +24,27 @@ class Navbar extends Component {
             </a>
             <ul className="right hide-on-small-only">
               <li>
-                <Link to="/">
+                <a href="/">
                   <i className="fa fa-user fa-3" aria-hidden="true" />
                   {this.state.name}
-                </Link>
+                </a>
               </li>
               <li>
-                <Link to="/logout">
+                <a href="/logout">
                   <i className="fa fa-sign-out" aria-hidden="true" />Log Out
-                </Link>
+                </a>
               </li>
             </ul>
             <ul className="side-nav" id="main-menu">
               <li>
-                <Link to="/">
+                <a href="/">
                   <i className="fa fa-users" /> Shoppinglists
-                </Link>
+                </a>
               </li>
               <li>
-                <Link to="/shoppinglist/add">
+                <a href="/shoppinglist/add">
                   <i className="fa fa-plus" /> Add ShoppingList
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
