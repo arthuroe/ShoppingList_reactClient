c53cd9c9ce3e9d0302b99035c11a240075700753
orache arthur <arthur.orache@gmail.com>
2017-12-20 22:23:02 +0300

    Fix import issue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Login from "./components/login.js";
+import Register from "./components/register.js";
+import Logout from "./components/logout.js";
+import ResetPassword from "./components/reset-password.js";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/logout" component={Logout} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/reset-password" component={ResetPassword} />
+        <App />
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
