8bd294a46f5ae96a8dbd4702147009f8ae1091f4
orache arthur <arthur.orache@gmail.com>
2017-12-21 14:43:17 +0300

    Fix url issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it("matches", () => {
+  const edit = shallow(<App />);
+  expect(shallowToJson(edit)).toMatchSnapshot();
 });
